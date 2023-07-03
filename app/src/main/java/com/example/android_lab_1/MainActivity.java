7a59cec2e9e9c58c0fcf361be34c78f2b55bb64b
nsty <kornas_01@mail.ru>
2023-07-03 21:15:49 +0300

    lab4

--- app/src/main/java/com/example/android_lab_1/MainActivity.java
+++ app/src/main/java/com/example/android_lab_1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android_lab_1;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,11 +13,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class MainActivity extends Activity {
     private EditText username;
     private EditText password;
     private TextView attempts;
     private TextView numberOfAttempts;
+
+    private SharedPreferences sharedPref;
+    private final String saveLoginKey = "save_login";
 
     int numberOfRemainingLoginAttempts = 5;
 
@@ -22,12 +30,16 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        restoreLocale();
         setContentView(R.layout.activity_main);
 
-        username = (EditText) findViewById(R.id.edit_user);
-        password = (EditText) findViewById(R.id.edit_password);
-        attempts = (TextView) findViewById(R.id.attempts);
-        numberOfAttempts = (TextView) findViewById(R.id.number_of_attempts);
+        sharedPref = this.getSharedPreferences("login", Context.MODE_PRIVATE);
+
+        // Связываемся с элементами нашего интерфейса:
+        username =  findViewById(R.id.edit_user);
+        password =  findViewById(R.id.edit_password);
+        attempts =  findViewById(R.id.attempts);
+        numberOfAttempts = findViewById(R.id.number_of_attempts);
         numberOfAttempts.setText(Integer.toString(numberOfRemainingLoginAttempts));
 
         Toast.makeText(MainActivity.this,
@@ -39,19 +51,24 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onStop(){
-        Log.i("AppLogger", "Переопределение onStop у MainActivity");
+        saveAuthorization();
         super.onStop();
-       // Log.i("AppLogger", "Переопределение onStop у MainActivity");
+        Log.i("AppLogger", "Переопределение onStop у MainActivity");
     }
     @Override
     protected void onStart(){
+        restoreAuthorization();
         super.onStart();
+
+
         Log.i("AppLogger", "Переопределение onStart у MainActivity");
     }
     @Override
     protected void onPause(){
+        Log.i("AppLogger", "Locale до паузы: " + Locale.getDefault());
         super.onPause();
         Log.i("AppLogger", "Переопределение onPause у MainActivity");
+        Log.i("AppLogger", "Locale после паузы: " + Locale.getDefault());
     }
     @Override
     protected void onResume(){
@@ -65,8 +82,13 @@ public class MainActivity extends Activity {
         Log.i("AppLogger", "Переопределение onRestart у MainActivity");
     }
 
+    // Обрабатываем нажатие кнопки "Войти":
     @SuppressLint("SetTextI18n")
-    public void Login(View view) {
+    public void login(View view) {
+        Log.i("login", "user login");
+
+        String name = username.getText().toString();
+        String pass = password.getText().toString();
 
         if (username.getText().toString().equals("anastasia") &&
                 password.getText().toString().equals("kornaukhova")) {
@@ -90,4 +112,68 @@ public class MainActivity extends Activity {
 
         }
     }
+
+    private void  saveAuthorization(){
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(saveLoginKey, username.getText().toString());
+        editor.apply();
+    }
+
+    private void restoreAuthorization(){
+        username.setText(sharedPref.getString(saveLoginKey,""));
+    }
+
+
+    public void changeLocale(View view){
+        String lang;
+        Locale current = Locale.getDefault();
+        if(Locale.getDefault().getLanguage().contains("en"))
+            lang = "ru";
+
+        else
+            lang = "en";
+
+        Locale myLocale = new Locale(lang);
+
+        Locale.setDefault(myLocale);
+        android.content.res.Configuration config = new android.content.res.Configuration();
+        config.locale = myLocale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+        saveLocale(lang);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+        overridePendingTransition(0, 0);
+    }
+
+    protected void saveLocale(String localeCode){
+        Locale current = Locale.getDefault();
+        if(current.getLanguage().contains("en"))
+            localeCode = "en";
+        else
+            localeCode = "ru";
+
+        SharedPreferences sharedLocPref = getSharedPreferences("locale", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedLocPref.edit();
+        editor.putString("locale_key", localeCode);
+        editor.apply();
+
+    }
+
+    protected void restoreLocale(){
+        SharedPreferences sharedLocPref = getSharedPreferences("locale", Context.MODE_PRIVATE);
+        String lang = sharedLocPref.getString("locale_key", "ru");
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getResources().updateConfiguration(
+                config,
+                getResources().getDisplayMetrics()
+        );
+    }
+
 }
--- app/src/main/java/com/example/android_lab_1/TableActivity.java
+++ app/src/main/java/com/example/android_lab_1/TableActivity.java
@@ -1,6 +1,7 @@
 package com.example.android_lab_1;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,23 +10,25 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Button;
-import android.content.Intent;
+
 import java.util.ArrayList;
 import java.util.Collections;
-import android.provider.MediaStore;
 
-public class TableActivity extends AppCompatActivity {
-
-    ArrayList<String> entries = new ArrayList<String>();
-    ArrayList<String> selectedEntries = new ArrayList<String>();
+public class TableActivity extends MainActivity {
+    ArrayList<String> entries = new ArrayList<>();
+    ArrayList<String> selectedEntries = new ArrayList<>();
     ArrayAdapter<String> adapter;
     ListView entriesList;
-    private Button cameraButton;
+
+    private SharedPreferences sharedPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        restoreLocale();
         setContentView(R.layout.activity_table);
+
+        sharedPref = this.getSharedPreferences("table", Context.MODE_PRIVATE);
 
         Collections.addAll(entries);
 
@@ -49,27 +52,17 @@ public class TableActivity extends AppCompatActivity {
                     selectedEntries.remove(entrie);
             }
         });
-
-        cameraButton = findViewById(R.id.cameraButton);
-        cameraButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(cameraIntent);
-                if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-                    //startActivity(cameraIntent);
-                }
-            }
-        });
     }
 
     @Override
     protected void onStop(){
+        saveTable();
         super.onStop();
         Log.i("AppLogger", "Переопределение onStop у TableActivity");
     }
     @Override
     protected void onStart(){
+        restoreTable();
         super.onStart();
         Log.i("AppLogger", "Переопределение onStart у TableActivity");
     }
@@ -100,4 +93,44 @@ public class TableActivity extends AppCompatActivity {
             adapter.notifyDataSetChanged();
         }
     }
+    public void removeEntrie(View view){
+
+        if(selectedEntries.size() == 0 && entries.size() != 0) {
+            int numOfLastEntrie = entries.size() - 1;
+            adapter.remove(entries.get(numOfLastEntrie));
+        }
+
+        for(int i=0; i < selectedEntries.size();i++){
+            adapter.remove(selectedEntries.get(i));
+        }
+        entriesList.clearChoices();
+        selectedEntries.clear();
+
+        adapter.notifyDataSetChanged();
+    }
+
+    private void saveTable(){
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putInt("table_size", entries.size());
+        for(int i=0;i < entries.size(); i++)
+        {
+            editor.remove("table_ent_" + i);
+            editor.putString("table_ent_" + i, entries.get(i));
+        }
+
+        editor.apply();
+    }
+
+    private void restoreTable()
+    {
+        entries.clear();
+        int size = sharedPref.getInt("table_size", 0);
+        for(int i=0;i < size;i++)
+        {
+            entries.add(sharedPref.getString("table_ent_" + i, null));
+        }
+
+    }
+
 }
